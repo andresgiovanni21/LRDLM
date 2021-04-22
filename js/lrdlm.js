452c3d115e982fe67c362ca0e08f803107e17d4d
andresgiovanni21 <andresgiovanni21@hotmail.com>
2021-04-21 20:56:46 -0500

    compilacion para entrega

--- js/lrdlm.js
+++ js/lrdlm.js
@@ -1,15 +1,16 @@
-//Primear funcion: Modifica el comportamiento de los botones del carrusel
+//Primera funcion: Modifica el comportamiento de los botones del carrusel
 $(document).ready(function(){
-    // Activate Carousel
+    //Activar slider
     $("#derecaro").carousel({interval: 4000, pause: false});
     $("#centerCaro").carousel({interval: 4000, pause: false});
     $("#izqcaro").carousel({interval: 4000, pause: false});
-
+    //Boton izquierdo
     $(".botonizq").click(function(){
         $("#derecaro").carousel("prev");
         $("#centerCaro").carousel("prev");
         $("#izqcaro").carousel("prev");
     });
+    //Boton derecho
     $(".botonader").click(function(){
         $("#derecaro").carousel("next");
         $("#centerCaro").carousel("next");
@@ -17,22 +18,24 @@ $(document).ready(function(){
     });
 });
 
-// Segunda funcion: Scroll progress de los modals
-
+// Segunda funcion: Scroll progress para cada modal
 $(".modal-body").scroll(function() {
     scrollito($(this));
 });
 
 function scrollito(mb){
-    let scroll = mb.scrollTop();
-    let scroll_height = mb.get(0).scrollHeight;
-    let height = mb.height();
-    let resta = scroll_height-(height*1.06);
-    let scrolled = (scroll / resta)*100;
-
-    document.getElementById("bar"+mb.attr("id")).style.width = scrolled + "%";
+    let escrol = mb.scrollTop();
+    let escrolaltura = mb.get(0).scrollHeight;
+    let alturamodal = mb.height();
+    let resta = escrolaltura-(alturamodal*1.06); //Multiplicar un poquito para que sea menor al escrol y llegue a 100%
+    let ancho = (escrol / resta)*100;
+    document.getElementById("bar"+mb.attr("id")).style.width = ancho + "%";
 }
 
-
-//Tercera funcion: Spiner de carga inicial
-
+//Tercera funcion: Spiner de carga inicial con 1 seg de delay
+$(window).on('load',function() {
+    setTimeout(function () {
+        $(".loader").fadeOut('slow');
+        $('body').removeClass('ocultar');//restaura el scroll del body
+    },1000)
+});
